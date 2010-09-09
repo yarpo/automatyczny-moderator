c850ade048edf329abce4fdf5ad56cf85ed266ba
Jar.Patryk <Jar.Patryk@f56d72db-52aa-ae01-5e67-d068f624f21c>
2010-09-09 18:33:34 +0000

    kominukaty powitalne i flow programu

--- trunk/automatyczny_moderator/Interface.cs
+++ trunk/automatyczny_moderator/Interface.cs
@@ -7,11 +7,30 @@ namespace automatyczny_moderator
 {
     class Interface
     {
-        public void appInfo()
+        private const string YES = "y";
+        private const string NO = "n";
+
+        public bool startModerator()
         {
             Console.WriteLine("Automatyczny moderator");
             Console.WriteLine("Patryk Jar");
             Console.WriteLine("wrzesien 2010 r.");
+            Console.WriteLine("");
+            
+            string dec = "";
+            while (YES != dec && NO != dec)
+            {
+                Console.WriteLine("Czy chcesz uruchomić automatycznego moderatora? y /n");
+                dec = Console.ReadLine();
+            }
+            return (YES == dec);
+        }
+
+        public void goodByeScreen()
+        {
+            Console.WriteLine("Dziękuję za skorzystanie z programu.");
+            Console.WriteLine("\nNaciśnij enter by zakończyć program...");
+            Console.Read();
         }
     }
 }
