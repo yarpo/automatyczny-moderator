769f37c3682212b2e2d3cf42156a96de9dd762a9
Jar.Patryk <Jar.Patryk@f56d72db-52aa-ae01-5e67-d068f624f21c>
2010-09-09 22:03:05 +0000

    dodanie tabeli dla logow usera

--- db/struct.sql
+++ db/struct.sql
@@ -3,4 +3,12 @@ CREATE TABLE `automod_mlog` (
   `date` datetime NOT NULL,
   `info` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-)
+);
+
+CREATE TABLE `automod_ulog` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `iduser` int(9) NOT NULL,
+  `date` datetime NOT NULL,
+  `info` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
