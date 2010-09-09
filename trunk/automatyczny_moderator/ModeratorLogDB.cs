59f12e952b602dcbbdd383491903a4478bf94d37
Jar.Patryk <Jar.Patryk@f56d72db-52aa-ae01-5e67-d068f624f21c>
2010-09-09 21:50:10 +0000

    wprowadzenie nowego interfejsu

--- trunk/automatyczny_moderator/ModeratorLogDB.cs
+++ trunk/automatyczny_moderator/ModeratorLogDB.cs
@@ -8,46 +8,51 @@ namespace automatyczny_moderator
 {
     public class ModeratorLogDB : ModeratorLog
     {
+        const string READING = "reading posts";
         const string READ_LAST_DATE = "last date reading...";
-        const string WRITE_LAST_DATE = "last date writing...";
+        const string START_OF_WORK = "start of moderation";
+        const string END_OF_WORK = "end of moderation this time";
 
         private const string TABLE = "automod_mlog";
         private const string DATE_NAME = "date";
         private const string INFO_NAME = "info";
 
-        private const int DATE_INDEX = 0;
-        private const int INFO_INDEX = 1;
+        private const int DATE_INDEX = 1;
+        private const int INFO_INDEX = 2;
 
         private const string THE_VERY_BEGINNING = "1970-01-01 00:00:01";
 
         #region ModeratorLog Members
         public string getLastModerationDate()
         {
+            log(READ_LAST_DATE);
             string sql = "SELECT " + DATE_NAME + " FROM " + TABLE;
             MySqlDataReader result = DatabaseSrc.query(sql);
-            if (result.Read())
-            {
-                return result.GetString(DATE_INDEX);
-            }
-            return THE_VERY_BEGINNING;
+
+            return (result.Read()) ? result.GetString(0) : THE_VERY_BEGINNING;
         }
 
-        public void setLastModerationDate()
+        public void startOfWork()
         {
-            DateTime saveNow = DateTime.Now;
-            setLastModerationDate(DateTime.Now.ToString());
+            log(START_OF_WORK);
         }
 
-        public void setLastModerationDate(string date)
+        public void reading()
         {
-            // zapisz do bazy danych
+            log(READING);
+        }
+
+
+        public void endOfWork()
+        {
+            log(END_OF_WORK);
         }
 
         public void log(string status)
         {
             string now = getCurrentDateString();
-            string sql = "INSERT INTO " + TABLE + 
-                " VALUES ('" + now + "', '"  + status + "')";
+            string sql = "INSERT INTO " + TABLE + "(id, date, info)" +
+                " VALUES (NULL, '" + now + "', '"  + status + "')";
             DatabaseSrc.query(sql);
         }
 
