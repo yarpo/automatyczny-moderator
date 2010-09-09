465836ce008102283d108dc6d4ebfbd695ce029c
Jar.Patryk <Jar.Patryk@f56d72db-52aa-ae01-5e67-d068f624f21c>
2010-09-09 19:26:17 +0000

    sprawne logowanie zachowań moderatora

--- automatyczny_moderator/DatabaseSrc.cs
+++ automatyczny_moderator/DatabaseSrc.cs
@@ -24,6 +24,9 @@ namespace automatyczny_moderator
                             ";uid=" + DB_USER +
                             ";password=" + DB_PASS
                 );
+            }
+            if (connection.State == System.Data.ConnectionState.Closed)
+            {
                 connection.Open();
             }
             return connection;
@@ -31,103 +34,20 @@ namespace automatyczny_moderator
 
         static public void closeConnection()
         {
-            getConnection().Close();
+            if (null != connection && connection.State == System.Data.ConnectionState.Open)
+            {
+                connection.Close();
+            }
         }
 
         static public MySqlDataReader query(string sql)
         {
+            closeConnection();
+            getConnection();
             MySqlCommand mysqlCmd = new MySqlCommand(sql, connection);
-
-            return mysqlCmd.ExecuteReader();
+            Console.WriteLine(sql);
+            MySqlDataReader result = mysqlCmd.ExecuteReader();
+            return result;
         }
     }
 }
-
-         /*
-            string strSQL = "SELECT * FROM phpbb_posts";
-            MySqlCommand mysqlCmd = new MySqlCommand(strSQL, connection);
-
-            //open the connection
-            connection.Open();
-    }
-}
-/*using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using NHunspell;
-using MySql.Data.MySqlClient;
-
-namespace proba_nhuspell
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            MySqlConnection connection = new MySqlConnection("server=localhost;database=forum;uid=moderator;password=qwe123");
-
-            string strSQL = "SELECT * FROM phpbb_posts";
-            MySqlCommand mysqlCmd = new MySqlCommand(strSQL, connection);
-
-            //open the connection
-            connection.Open();
-
-            MySqlDataReader mysqlReader = mysqlCmd.ExecuteReader();
-            while (mysqlReader.Read())
-            {
-                Console.WriteLine(mysqlReader.GetInt32(0) + "\t" +
-                    mysqlReader.GetString(15) + "\t" + mysqlReader.GetString(16));
-            }
-
-            //close the connection
-            connection.Close();
-
-            Console.WriteLine("NHunspell functions and classes demo");
-
-            Console.WriteLine("");
-            Console.WriteLine("Spell Check with with Hunspell");
-
-            // Important: Due to the fact Hunspell will use unmanaged memory
-            // you have to serve the IDisposable pattern
-            // In this block of code this is be done
-            // by a using block. But you can also call hunspell.Dispose()
-            using (NHunspell.Hunspell hunspell = new Hunspell("pl_pl.aff", "pl_pl.dic"))
-            {
-                Console.WriteLine("Sprawdzam slowo 'rekomendacja'");
-                bool correct = hunspell.Spell("Rekomendacja");
-                Console.WriteLine("Rekomendacja jest napisana " +
-                          (correct ? "porawnie" : "niepoprawnie"));
-
-                Console.WriteLine("");
-                Console.WriteLine("Sprawdzmy dla 'Rekomendaca'");
-                List<string> suggestions = hunspell.Suggest("Rekomendaca");
-                Console.WriteLine("Sa sugerowane slowa " + suggestions.Count.ToString());
-                foreach (string suggestion in suggestions)
-                {
-                    Console.WriteLine("Podpowiedz: " + suggestion);
-                }
-            }
-
-            Console.WriteLine("");
-            Console.WriteLine("Hyphenation with Hyph");
-
-            // Important: Due to the fact Hyphen will use unmanaged
-            // memory you have to serve the IDisposable pattern
-            // In this block of code this is be done by a using block.
-            // But you can also call hyphen.Dispose()
-            using (NHunspell.Hyphen hyphen = new Hyphen("hyph_en_us.dic"))
-            {
-                Console.WriteLine("Get the hyphenation of the word 'Recommendation'");
-                NHunspell.HyphenResult hyphenated = hyphen.Hyphenate("Recommendation");
-                Console.WriteLine("'Recommendation' is hyphenated as: " +
-                                  hyphenated.HyphenatedWord);
-            }
-
-            Console.WriteLine("");
-            Console.WriteLine("Press any key to continue...");
-            Console.ReadKey();
-
-        }
-    }
-}
-*/
